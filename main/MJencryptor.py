25a80143dc676eed580f16662f693328aa57f60e
Jonatas_PCSENAI <jonatas.miguelss@gmail.com>
2021-12-10 14:03:25 -0300

    Adição de try e except no código main

--- main/MJencryptor.py
+++ main/MJencryptor.py
@@ -20,7 +20,10 @@ def main():
     line_command = argparse.ArgumentParser(description= "|MJencryptor|\nEncriptador de arquivos texto")
     line_command.add_argument('-path','-p',required=True,help= "Caminho ou nome do arquivo (caso esteja no mesmo diretorio)")
     args = line_command.parse_args()
-    path_r = open(args.path,'r')
-    print("Caminho do arquivo:{}".format(path_r))
+    try:
+        path_r = open(args.path,'r')
+        print("Caminho do arquivo:{}".format(path_r))
+    except FileNotFoundError:
+        print("Arquivo não encontrado!\nDigite o diretório ou nome correto do arquivo")
 
 main()
