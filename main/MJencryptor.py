87d4b668c1b3032db8729e31cfab14f80a26bab3
Jonatas <jonatas.miguelss@gmail.com>
2021-12-11 00:58:00 -0300

    Adiçao da funçao cripto ao main, criaçao de anim de loading

--- main/MJencryptor.py
+++ main/MJencryptor.py
@@ -15,13 +15,13 @@ import os
 import sys
 import argparse
 from typing import get_args
+import time
 
 def cripto(arquivo = None,output = None):
     cript = {"a":"zx2","e":"cvl","i":"2=z","o":"=A|","u":"0+i"," ":"=+"}
     content_arq = open(arquivo,'r')
     out_file = open(output,'w+')
     saida = content_arq.readlines()
-    i = 0
     for line in saida:
         saida_line = line
         for letter in saida_line:
@@ -29,25 +29,21 @@ def cripto(arquivo = None,output = None):
                 saida_line = saida_line.replace(letter,cript[letter])
         print(saida_line)
         out_file.writelines(saida_line)
-    
-    '''
-    for l in nome:
-        if l in cript:
-            n_pos = saida.find(l)
-            saida = saida.replace(l,cript[l])
-            
-        else:
-            pass
-    print(saida)
-    '''
+
+
 def main():
     line_command = argparse.ArgumentParser(description= "|MJencryptor|\nEncriptador de arquivos texto")
     line_command.add_argument('-path','-p',required=True,help= "Caminho ou nome do arquivo (caso esteja no mesmo diretorio)")
+    line_command.add_argument('-output','-o', help='Cria um arquivo contendo o texto criptografado!')
     args = line_command.parse_args()
     try:
-        path_r = open(args.path,'r')
-        print("Caminho do arquivo:{}".format(path_r))
+        cripto(str(args.path),str(args.output))
+        print("Caminho adicionado!")
+        anim = "="
+        for i in range(1,20):
+            print("Criptografando {}".format(anim*i))
+            time.sleep(0.1)
+        print("="*15,"Concluido!","="*15)
     except FileNotFoundError:
         print("ARQUIVO NÃO ENCONTRADO\nVERIFIQUE O NOME OU O CAMINHO!")
-
-cripto('arquivo_teste.txt','said.txt')
+main()
