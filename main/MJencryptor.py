18ba3084605a857da97fdbfaed6678bef7a741af
Jonatas <jonatas.miguelss@gmail.com>
2021-12-11 00:33:04 -0300

    Finalizaçao de codificaçao de arquivos txt

--- main/MJencryptor.py
+++ main/MJencryptor.py
@@ -16,10 +16,27 @@ import sys
 import argparse
 from typing import get_args
 
-def cripto(nome_arquivo = None):
-    biblioteca = {}
-
-
+def cripto(arquivo = None):
+    cript = {"a":"zx2","e":"cvl","i":"2=z","o":"=A|","u":"0+i"," ":"=+"}
+    content_arq = open(arquivo,'r')
+    saida = content_arq.readlines()
+    i = 0
+    for line in saida:
+        saida_line = line
+        for letter in saida_line:
+            if letter in cript:
+                saida_line = saida_line.replace(letter,cript[letter])
+        print(saida_line)
+    '''
+    for l in nome:
+        if l in cript:
+            n_pos = saida.find(l)
+            saida = saida.replace(l,cript[l])
+            
+        else:
+            pass
+    print(saida)
+    '''
 def main():
     line_command = argparse.ArgumentParser(description= "|MJencryptor|\nEncriptador de arquivos texto")
     line_command.add_argument('-path','-p',required=True,help= "Caminho ou nome do arquivo (caso esteja no mesmo diretorio)")
@@ -30,3 +47,4 @@ def main():
     except FileNotFoundError:
         print("ARQUIVO NÃO ENCONTRADO\nVERIFIQUE O NOME OU O CAMINHO!")
 
+cripto('arquivo_teste.txt')
